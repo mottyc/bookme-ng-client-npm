9f8c03fdb0df76542be4a3aee9355788e845d87f
mottyc <motty.cohen@gmail.com>
2020-08-23 15:29:31 +0000

    updated lib

--- lib/entities/Placement.d.ts
+++ lib/entities/Placement.d.ts
@@ -1,9 +1,10 @@
-import { Resource } from '../entities/Resource';
 import { Booking } from '../entities/Booking';
 import { PlacementStatusCode } from '../enums/PlacementStatusCode';
 import { BaseEntity } from '../entities/BaseEntity';
+import { Resource } from '../entities/Resource';
 import { IBaseEntity } from '../entities/BaseEntity';
 export declare class Placement extends BaseEntity {
+    activityId: string;
     resource: Resource;
     bookings: Booking[];
     status: PlacementStatusCode;
@@ -12,6 +13,7 @@ export declare class Placement extends BaseEntity {
     updatedOn: number;
 }
 export interface IPlacement extends IBaseEntity {
+    activityId?: string;
     resource?: Resource;
     bookings?: Booking[];
     status?: PlacementStatusCode;
